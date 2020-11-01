6ab35b2b8a3df190f91468be576de057985b7800
Adam R Moss <adamrmoss@gmail.com>
2020-11-01 13:13:43 -0500

    minor

--- build.ts
+++ build.ts
@@ -2,7 +2,7 @@ import { getCommandLineOptions, showHelp } from './command-line-options';
 import { ensureOutputDirectory, buildRom, runRom } from './dasm';
 
 const options = getCommandLineOptions();
-let { help, name, run } = options;
+const { help, name, run } = options;
 
 if (help || !name)
 {
