413657c2977f2a2e06f00fd48d5ca09fc0c311be
Adam R Moss <adamrmoss@gmail.com>
2020-11-17 13:53:11 -0500

    Include Math namespace in local scope

--- cli/generate.ts
+++ cli/generate.ts
@@ -4,6 +4,15 @@ import { ensureDirectories } from './paths';
 import { ICommandLineOptions, optionsDefinitions, usageSections } from './generate/command-line-options';
 import { getNumberData } from './generate/data-size';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const {
+    abs, acos, acosh, asin, asinh, atan, atanh, atan2,
+    cbrt, ceil, clz32, cos, cosh, exp, expm1, floor, fround,
+    hypot, imul, log, log1p, log10, log2, max, min, pow,
+    random, round, sign, sin, sinh, sqrt, tan, tanh, trunc,
+    E, LN2, LN10, LOG2E, LOG10E, PI, SQRT1_2, SQRT2,
+} = Math;
+
 const options = getCommandLineOptions<ICommandLineOptions>(optionsDefinitions);
 const { help, name, expression, length, dataSize } = options;
 
